ececf997064db07905705d71be2b22bcca86aba7
cvassalos <vassalce@gmail.com>
2024-12-06 19:27:34 -0500

    Messing with the TKinter main window

--- bVar.py
+++ bVar.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
+root.title("Messing with BooleanVar")
+root.geometry("300x300")
 
 # Using BooleanVar to store state
 button_state = BooleanVar(value=False)
@@ -16,5 +18,12 @@ def print_state():
 
 button = ttk.Button(root, text="Print State", command=print_state)
 button.grid()
+
+def print_hierarchy(w, depth=0):
+    print('  '*depth + w.winfo_class() + ' w=' + str(w.winfo_width()) + ' h=' + str(w.winfo_height()) + ' x=' + str(w.winfo_x()) + ' y=' + str(w.winfo_y()))
+    for i in w.winfo_children():
+        print_hierarchy(i, depth+1)
+
+print_hierarchy(root)
 
 root.mainloop()
