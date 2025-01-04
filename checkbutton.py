3d1c2e1a492048f79acac9c53142885cbab8a32e
cvassalos <vassalce@gmail.com>
2025-01-03 19:54:28 -0500

    Created new file to mess with checkbuttons in tkinter

---
+++ checkbutton.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.geometry("400x300")
+root.title("Checkbutton")
+
+measureSystem = StringVar()
+check = ttk.Checkbutton(root, text='Use Metric',
+                        variable=measureSystem,
+                        onvalue='metric', offvalue='imperial').grid()
+
+
+root.mainloop()
